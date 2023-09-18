3e20cd1824c78152c3697b20687639a58926e874
silkhollin <silcollinz@gmail.com>
2023-09-17 21:30:20 -0700

    builitin functions and issues

---
+++ built-in_handle.c
@@ -0,0 +1,54 @@
+#include "shell.h"
+
+/**
+ * help_env - Displays information on the built-in command 'env'.
+ */
+void help_env(void)
+{
+	char *msg = "env: env\n\tPrints the current environment.\n";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
+
+/**
+ * help_setenv - Displays information on the shell built-in command 'setenv'.
+ */
+void help_setenv(void)
+{
+	char *msg = "setenv: setenv [VARIABLE] [VALUE]\n\tInitializes a new";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "environment variable, or modifies an existing one.\n\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\tUpon failure, prints a message to stderr.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
+
+/**
+ * help_unsetenv - Displays information on the shell built-in command
+ * 'unsetenv'.
+ */
+void help_unsetenv(void)
+{
+	char *msg = "unsetenv: unsetenv [VARIABLE]\n\tRemoves an ";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "environmental variable.\n\n\tUpon failure, prints a ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "message to stderr.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
+
+/**
+ * help_help - Shows information on the shell built-in command 'help'.
+ */
+void help_help(void)
+{
+	char *msg = "help: help\n\tSee all possible Shellby builtin commands.\n";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\n      help [BUILTIN NAME]\n\tSee specific information on each ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "builtin command.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
